5896c61b071b070ac8f1e2ef85aa7a788c4eaee0
drarunas <104825222+drarunas@users.noreply.github.com>
2024-02-25 19:41:34 +0000

    randomization

--- script.js
+++ script.js
@@ -20,6 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 });
 
+document
+    .getElementById("randomizeButton")
+    .addEventListener("click", function () {
+        // Shuffle the emojis array
+        emojis = shuffleArray(emojis);
+        // Display the shuffled emojis
+        displayEmojis(emojis);
+    });
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+}
+
 function attachSearchListener() {
     const searchInput = document.getElementById("emojiSearch");
     searchInput.addEventListener("input", function () {
